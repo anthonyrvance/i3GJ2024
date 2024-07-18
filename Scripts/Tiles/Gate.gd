26d1190973e45691f20ca1f93e7113712d4ec01f
anthonyrvance <48871259+anthonyrvance@users.noreply.github.com>
2024-07-17 21:30:04 -0500

    prev commit included fixing refill bug, player score/size hopefully makes more sense, use snow scaling, and painted a level 1 v1

--- Scripts/Tiles/Gate.gd
+++ Scripts/Tiles/Gate.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var reqSize = 3
+@export var levelPathToLoad = ""
 
 func _on_body_entered(body):
 	var player := body as Player
@@ -9,5 +10,6 @@ func _on_body_entered(body):
 		
 	if player.get_size() == reqSize:
 		print("players size: " + str(player.get_size()) + "  which needs to be: " + str(reqSize) + " = SUCCESS")
+		get_tree().change_scene_to_file(levelPathToLoad)
 	else:
 		print("players size: " + str(player.get_size()) + " which needs to be: " + str(reqSize) + " = FAIL")
--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -4,4 +4,4 @@ func _ready():
 	BackgroundMusicPlayer.play_music_level()
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Levels/Sandbox.tscn")
+	get_tree().change_scene_to_file("res://Levels/Level 1.tscn")
