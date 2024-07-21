d655e565407368ebef7ad949b6eb9dbc5b26cb1c
anthonyrvance <48871259+anthonyrvance@users.noreply.github.com>
2024-07-21 17:22:17 -0500

    buttons, font, menus

--- Scripts/UI/PauseMenu.gd
+++ Scripts/UI/PauseMenu.gd
@@ -1,22 +1,18 @@
 extends Control
 
-func _on_quit_confirm_button_pressed():
-	get_tree().change_scene_to_file("res://Levels/MainMenu.tscn")
-
-
-func _on_close_pause_menu_button_pressed():
+func _on_resume_play_button_pressed():
 	queue_free()
 
+func _on_open_settings_button_pressed():
+	print("make settings pls")
 
-func _on_settings_button_pressed():
-	pass # Replace with function body.
-
-
-func _on_open_quit_menu_button_pressed():
+func _on_open_exit_menu_button_pressed():
 	$Exit.visible = true
 	$Pause.visible = false
 
+func _on_quit_level_button_pressed():
+		get_tree().change_scene_to_file("res://Levels/MainMenu.tscn")
 
-func _on_close_escape_menu_button_pressed():
+func _on_return_button_pressed():
 	$Exit.visible = false
 	$Pause.visible = true
