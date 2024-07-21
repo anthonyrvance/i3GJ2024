0a9be7b9a598bbe06656b10ada5ed229e060045d
anthonyrvance <48871259+anthonyrvance@users.noreply.github.com>
2024-07-21 14:33:36 -0500

    menu changes

--- Scripts/UI/MainMenu.gd
+++ Scripts/UI/MainMenu.gd
@@ -3,5 +3,11 @@ extends Node2D
 func _ready():
 	BackgroundMusicPlayer.play_music_level()
 
-func _on_button_pressed():
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Levels/Level 1.tscn")
+
+func _on_settings_button_pressed():
+	print("make settings pls")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
